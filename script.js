ad167b2720b7cb7a42ccc09b5bf18a062e43d381
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-01 00:46:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function paint(color) {
     cellColors[0] = colorCode;
     document.getElementById('cell-0').style.backgroundColor = colorCode;
     document.getElementById('cell-0').textContent = currentNumber; // Установка номера в первую ячейку
-    document.getElementById('cell-0').textContent = colorCode === '#FF0000' ? '' : currentNumber;
+    document.getElementById('cell-0').textContent = colorCode === '#FF0000' ? currentNumber: ";
 
 
     if (colorCode === '#00FF00') {
