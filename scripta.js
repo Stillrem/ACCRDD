718b64dcfbb2189bfbee365f1b2ce3f81259284d
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-14 15:03:40 -0800

    Update scripta.js

--- scripta.js
+++ scripta.js
@@ -53,19 +53,15 @@ function setRandomColors() {
 setRandomColors();
 setInterval(setRandomColors, 20000); // Меняем цвета каждые 20 секунд
 
-const mainPage = document.getElementById('mainPage');
-const adjustmentPage = document.getElementById('adjustmentPage');
-const adjustmentButton = document.getElementById('adjustmentButton');
-const backButton = document.getElementById('backButton');
+        // Анимация появления страницы
+        window.onload = function() {
+            document.body.style.opacity = '1';
+        };
 
-adjustmentButton.addEventListener('click', () => {
-    mainPage.classList.add('hidden');
-    adjustmentPage.classList.remove('hidden');
-    adjustmentPage.classList.add('page-transition');
-});
-
-backButton.addEventListener('click', () => {
-    adjustmentPage.classList.add('hidden');
-    mainPage.classList.remove('hidden');
-    mainPage.classList.add('page-transition');
-});
+        // Обработка кнопки "Назад"
+        document.getElementById('backButton').addEventListener('click', function() {
+            document.body.style.opacity = '0';
+            setTimeout(function() {
+                window.location.href = 'index.html';
+            }, 500);
+        });
