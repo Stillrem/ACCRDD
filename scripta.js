b5a0eb89386302d1cf8074af72085b35e3181dbd
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-17 21:21:05 -0800

    Update scripta.js

--- scripta.js
+++ scripta.js
@@ -21,15 +21,15 @@ function calculate() {
 }
 
 // Регистрация service worker
-if ('serviceWorker' in navigator) {
-                navigator.serviceWorker.register('/service-worker.js')
-                .then(registration => {
-                    console.log('Service Worker registered with scope:', registration.scope);
-                })
-                .catch(error => {
-                    console.error('Service Worker registration failed:', error);
-                });
-            }
+//if ('serviceWorker' in navigator) {
+                //navigator.serviceWorker.register('/service-worker.js')
+                //.then(registration => {
+                    //console.log('Service Worker registered with scope:', registration.scope);
+                //})
+                //.catch(error => {
+                    //console.error('Service Worker registration failed:', error);
+                //});
+            //}
 
             document.addEventListener('dblclick', function(event) {
                 event.preventDefault();
