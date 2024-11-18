763e66603b865749e43963a7335ae75b4bf87ba6
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-17 21:20:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -131,15 +131,15 @@ window.onload = function() {
                 updateDisplayCounts();
             });
 
-           // if ('serviceWorker' in navigator) {
-                //navigator.serviceWorker.register('/service-worker.js')
-                //.then(registration => {
-                    //console.log('Service Worker registered with scope:', registration.scope);
-                //})
-                //.catch(error => {
-                    //console.error('Service Worker registration failed:', error);
-                //});
-           //}
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker registered with scope:', registration.scope);
+                })
+                .catch(error => {
+                    console.error('Service Worker registration failed:', error);
+                });
+           }
 
             document.addEventListener('dblclick', function(event) {
                 event.preventDefault();
