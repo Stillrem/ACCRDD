8616ea28ac62d889ced2c91faec543c1d4d7bca2
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-17 20:08:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -21,15 +21,6 @@ function updateDisplayCounts() {
     localStorage.setItem('declineCount', declineCount);
 }
 
-        function calculatePay() {
-            const hours = parseFloat(document.getElementById('hours').value) || 0;
-            const minutes = parseFloat(document.getElementById('minutes').value) || 0;
-            const basePay = parseFloat(document.getElementById('basePay').value) || 0;
-          
-            const pay = 23 * (hours + (minutes * 1.6666 / 100)) - basePay;
-            document.getElementById('result').innerText = `Результат: ${pay.toFixed(2)}`;
-        }
-
 function paint(color) {
     const colorCode = color === 'red' ? '#FF0000' : '#00FF00';
 
