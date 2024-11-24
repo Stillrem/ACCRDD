ac2f440f6134f8685daa2cee2b7436c1f09abcef
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-11-24 11:54:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -217,3 +217,35 @@ document.getElementById('adjustmentButton').addEventListener('click', function()
         window.location.href = 'adjustment.html';
     }, 300);
 });
+
+   document.addEventListener('touchstart', handleTouchStart, false);
+   document.addEventListener('touchmove', handleTouchMove, false);
+
+   let xDown = null;
+   let yDown = null;
+
+   function handleTouchStart(evt) {
+       const firstTouch = evt.touches[0];
+       xDown = firstTouch.clientX;
+       yDown = firstTouch.clientY;
+   };
+
+   function handleTouchMove(evt) {
+       if (!xDown || !yDown) {
+           return;
+       }
+
+       const xUp = evt.touches[0].clientX;
+       const yUp = evt.touches[0].clientY;
+
+       const xDiff = xDown - xUp;
+       const yDiff = yDown - yUp;
+
+       if (Math.abs(xDiff) > Math.abs(yDiff)) {
+           // Свайп влево или вправо
+           evt.preventDefault();
+       }
+       xDown = null;
+       yDown = null;
+   };
+   
